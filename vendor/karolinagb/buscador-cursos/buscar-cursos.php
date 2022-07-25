1b0b46fa3a18b631eeefbdf1228dd320fe2910ec
x <karolina_bp@hotmail.com>
2022-07-25 11:32:55 -0300

    Utilizando pacote de terceiro

---
+++ vendor/karolinagb/buscador-cursos/buscar-cursos.php
@@ -0,0 +1,24 @@
+#!/usr/bin/env php
+
+<?php
+
+require 'vendor/autoload.php';
+
+// Teste::teste();
+// exit();
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false, 'base_uri' => 'https://www.alura.com.br/']);
+
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as  $curso) {
+    //lendo o texto do html selecionado
+    exibeMensagem($curso . PHP_EOL);
+}
